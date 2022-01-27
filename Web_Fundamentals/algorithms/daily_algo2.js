9b959dc6605b3f8f648ca2db804ae1451c9284b7
Brandon_Hart <sugb73@gmail.com>
2022-01-27 13:31:34 -0500

    Assignment Completion

--- Web_Fundamentals/algorithms/daily_algo2.js
+++ Web_Fundamentals/algorithms/daily_algo2.js
@@ -1,3 +1,4 @@
+// ##################################################################
 var x = 0;
 for(var i=1; i<5; i++) {
     x += i;
@@ -32,3 +33,35 @@ console.log(result2);
 
 // ####################################################################
 
+var x = 5;  
+// var x is a global variable
+
+function setX(newValue) {
+    x = newValue;
+}
+console.log(x);
+setX(15);
+console.log(x);
+    // Functions are a series of steps that accomplish a task.
+    // Function declaration starts with the "function" keyword, 
+        // followed by the name of the function.
+    // If you create a new variable, it can only be used inside that function.
+
+
+
+// ####################################################################
+var x = 5;
+    
+function addToX(amount) {
+    return x + amount;
+    console.log("hello there"); 
+    // the console.log will not print because there is a return above it
+    // the value of a function is whatever that function RETURNS
+}
+
+console.log(x);
+var result = addToX(-10);
+console.log(result);
+console.log(x);
+
+// ####################################################################
--- Web_Fundamentals/loop_challenges/loop_challenges.js
+++ Web_Fundamentals/loop_challenges/loop_challenges.js
@@ -18,3 +18,19 @@ for (var i = 5.5; i > -4; i -= 1.5) {
 }
 
 // Write code that will add all of the values from 1-100 onto some variable sum and at the end console.log the result 1 + 2 + 3 + ... + 98 + 99 + 100. We should get back 5050 at the end.######################################################################################
+
+var sum = 0
+for(var i = 0; i < 101; i++) {
+    sum = sum + i
+}
+console.log(sum)
+
+
+// Write code that will multiply all of the values from 1-12 onto some variable product and at the end console.log the result 1 * 2 * 3 * ... * 10 * 11 * 12. We should get back 479001600 at the end.######################################################################
+
+var product = 1
+
+for(var i = 1; i <= 12; i++) {
+    product *= i
+}
+console.log(product)
