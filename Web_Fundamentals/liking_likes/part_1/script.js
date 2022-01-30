e29f77fbca0890c0d61d773efdf1de8e8a8f727a
Brandon_Hart <sugb73@gmail.com>
2022-01-30 11:48:12 -0500

    Completed Assignments

--- Web_Fundamentals/liking_likes/part_1/script.js
+++ Web_Fundamentals/liking_likes/part_1/script.js
@@ -1,6 +1,10 @@
+function addLikes() {
+    var element = document.getElementById("likes_section_1");
+    var value = element.innerHTML;
 
+    value++;
 
-function addLikes(element) {
-    console.log(addLikes)
-
+    console.log(value)
+    document.getElementById("likes_section_1").innerHTML = value
 }
+
