e164ad7f472ea2dfd11c1ad39e1abee621cb80f2
Brandon_Hart <sugb73@gmail.com>
2022-01-31 14:06:52 -0500

    Completed Assignments

--- Web_Fundamentals/algorithms/daily_algo2.js
+++ Web_Fundamentals/algorithms/daily_algo2.js
@@ -82,3 +82,52 @@ function reverse(array) {
 reverse(array);
 
 // ####################################################################
+
+var floor = Math.floor(1.8);
+var ceiling = Math.ceil( Math.PI );
+// random returns a float inclusive of 0 and less than 1
+var random = Math.random();
+    
+console.log(floor); /* -->                   */
+console.log(ceiling);
+console.log(random);
+
+// Math.floor - Math is a class because it starts with a capital letter.
+
+// ####################################################################
+
+function d6(max) {
+    var roll = Math.random();
+    roll = Math.floor(Math.random() * max) + 1;
+    return roll;
+}
+    for(var i = 0; i < 100; i++) {
+    var playerRoll = d6(7);
+    console.log("The player rolled: " + playerRoll);
+}
+// ####################################################################
+// Using the following array, write a function that will answer all of our questions by randomly choosing a response
+
+var lifesAnswers = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes – definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful."
+];
+
