4bfe0369822e663377f9861c9518ecd986cd3082
Brandon_Hart <sugb73@gmail.com>
2022-02-08 13:34:42 -0500

    Completed Assignments and Bug Fixes

--- Python/fundamentals/fundamentals/bank_account/bank_account.py
+++ Python/fundamentals/fundamentals/bank_account/bank_account.py
@@ -1,17 +1,36 @@
 class BankAccount:
     def __init__(self, int_rate, balance):
-        self.int_rate = 0
-        self.balance = 0
+        self.int_rate = int_rate
+        self.balance = balance
 
     def deposit(self, amount):
         self.balance += amount
+        print(f"Total after deposit = {self.balance}")
+        return self
 
     def withdraw(self, amount):
-        self.balance -= amount
+        if amount <= 0 or self.balance - amount <= 0:
+            print("Insufficient funds")
+        else:
+            self.balance -= amount
+        print(f"Total after withdrawel = {self.balance}")
+        return self
 
     
     def display_account_info(self):
+        print("Balance " + self.balance)
 
 
     def yield_interest(self):
+        if self.balance > 0:
+            self.balance = self.balance + (self.balance * self.int_rate)
+            print(f"After interest = {self.balance}")
 
+
+# print("anything")
+billy = BankAccount(.01, 50)
+bob = BankAccount(.01, 75)
+
+billy.deposit(100).deposit(200).deposit(300).withdraw(200).yield_interest()
+
+bob.deposit(200).deposit(300).withdraw(100).withdraw(200).withdraw(300).withdraw(400).yield_interest()
--- Python/fundamentals/fundamentals/functions_intermediate_i/functions_intermediate_i.py
+++ Python/fundamentals/fundamentals/functions_intermediate_i/functions_intermediate_i.py
@@ -11,6 +11,10 @@ z = [{"x": 10, "y": 20}]
 #########################################################
 # Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
 
+# Out of range
+
+
+#############################################################
 x[1][0] = 15;
 print(x)
 
@@ -18,7 +22,7 @@ print(x)
 #########################################################
 # Change the last_name of the first student from 'Jordan' to 'Bryant'
 
-students = {"first_name": "Michael", "last_name": "Bryant"}
+students[0]["last_name"]="Bryant"
 print(students)
 
 #########################################################
@@ -34,3 +38,28 @@ print(sports_directory)
 
 z = [{"x": 10, "y": 30}]
 print(z)
+
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+    def iterateDictionary2(students):
+    for n in range(4):
+        print("first_name " + students[n]["first_name"] + "last_name" + students[n]["last_name"])
+
+
+
+# Where is the rest of my code?!?!?!?!?!?!?!?!?!?!?!?!?!?!
+
+
+# iterateDictionary(students) 
+
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+
